659497e01a5274f8d6952c3b4a56e7b1076b9abb
Hubert Iwaniuk <neotyk@kungfoo.pl>
2009-03-16 01:37:25 +0100

    Simple servlet registration. Now if you do "mvn clean install pax:provision" you can go to http://localhost:8080/hello and get response from just registered servlet.

--- src/main/java/pl/kungfoo/grizzly/osgi/httpservice/gitorial/impl/RegisterServletComponent.java
+++ src/main/java/pl/kungfoo/grizzly/osgi/httpservice/gitorial/impl/RegisterServletComponent.java
@@ -1,11 +1,23 @@
 package pl.kungfoo.grizzly.osgi.httpservice.gitorial.impl;
 
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 import org.osgi.service.http.*;
 
 public class RegisterServletComponent {
 
-    public void setHttp(HttpService http) {
-        System.out.println("Got Http Service: " + http);
+    public void setHttp(HttpService http) throws ServletException, NamespaceException {
+        http.registerServlet("/hello", new RegisterServlet(), null, null);
+    }
+}
+
+class RegisterServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest rq, HttpServletResponse rsp) throws IOException {
+        PrintWriter pw = rsp.getWriter();
+        pw.println("Hello World");
+        rsp.setContentType("text/plain");
     }
 }
 
